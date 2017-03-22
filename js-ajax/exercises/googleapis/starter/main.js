9bc2eb5ab334cbbffb6ce5b619eededf1b6ad32e
Mike Dang <mike@packstack.co>
2017-03-22 10:40:43 -0500

    Update main.js

--- js-ajax/exercises/googleapis/starter/main.js
+++ js-ajax/exercises/googleapis/starter/main.js
@@ -1,1 +1,13 @@
 console.log('main.js loaded');
+
+var googleBooksUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+
+$(document).ready(function() {
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+
+    var isbn = $('#isbn').val();
+
+    // Ajax code goes here
+  });
+});
